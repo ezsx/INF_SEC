c0e4b458590b9adaed79c4b34d59bd1d787860d7
ezsx <scdcor@gmail.com>
2022-09-22 14:57:42 +0300

    complete LR4, too easy?

---
+++ LR_4._var3.py
@@ -0,0 +1,45 @@
+class Network_member():
+    NM_list = []
+    Comp_list = []
+
+    def __init__(self, id: int, points: tuple):
+        self.id = id
+        self.points = points
+        Network_member.NM_list.append(self)
+
+    def __str__(self):
+        return f"ID: {self.id}, points: {self.points}, compatibility: {self.compatibility}"
+
+    @classmethod
+    def generate_compatibility(cls):
+        for i in cls.NM_list:
+            for j in cls.NM_list[cls.NM_list.index(i) + 1:]:
+                a1, b1, c1 = i.points
+                a2, b2, c2 = j.points
+                ap, bp, cp = abs(a1 - a2), abs(b1 - b2), abs(c1 - c2)
+
+                if ap == 0 and (bp != 0 and cp != 0) and (bp or cp) != 0:
+                    cls.Comp_list.append(("a", (i.id, j.id)))
+                if bp == 0 and (ap != 0 and cp != 0) and (ap or cp) != 0:
+                    cls.Comp_list.append(("b", (i.id, j.id)))
+                if cp == 0 and (bp != 0 and ap != 0) and (bp or ap) != 0:
+                    cls.Comp_list.append(("c", (i.id, j.id)))
+                else:
+                    cls.Comp_list.append((None, (i.id, j.id)))
+
+
+if __name__ == '__main__':
+    test_input = [
+        [1, (1, 2, 3)],
+        [2, (1, 4, 5)],
+        [3, (1, 2, 4)]
+    ]
+    for i in test_input:
+        Network_member(i[0], i[1])
+    Network_member.generate_compatibility()
+    count = 0
+    for i in Network_member.Comp_list:
+        print(i)
+        if i[0] is not None:
+            count += 1
+    print(count)
